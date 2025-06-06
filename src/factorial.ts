d68f3b0643c6489d79a90a0dda0969f1141e96cd
lucas4w <lucasdemoraes313@gmail.com>
2025-06-06 01:55:12 +0000

    .

--- src/factorial.ts
+++
@@ -1,9 +0,0 @@
-export function factorial(n: number): number {
-    if (n < 0) {
-        throw new Error("fatorial não definido para números negativos");
-    }
-    if (n === 0 || n === 1) {
-        return 1;
-    }
-    return n * factorial(n - 1);
-}
