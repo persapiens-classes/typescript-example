ac7562fcdd8085d66e1427aa22d9ed768d54e934
Beto <164033637+luiizr@users.noreply.github.com>
2025-06-06 16:47:19 +0000

    Testes implementados

---
+++ src/fatorial.test.ts
@@ -0,0 +1,15 @@
+import { fatorial } from '../src/fatorial';
+
+describe('Função fatorial', () => {
+  test('Deve retornar 120 para entrada 5', () => {
+    expect(fatorial(5)).toBe(120);
+  });
+
+  test('Deve retornar 1 para entrada 0', () => {
+    expect(fatorial(0)).toBe(1);
+  });
+
+  test('Deve lançar erro para número negativo', () => {
+    expect(() => fatorial(-3)).toThrow("Número negativo não é permitido.");
+  });
+});
