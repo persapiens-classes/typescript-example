c6d31c5a038c56f645ab88c7f1849695f170d719
Luiz Felipe <167254490+llopes05@users.noreply.github.com>
2025-06-06 16:40:11 +0000

    fix: consertando o fat.test e o fat para que números negativos deem erro

--- src/fat.test.ts
+++ src/fat.test.ts
@@ -11,3 +11,7 @@ test('fatorial de 2 deve ser 2', () => {
 test('fatorial de 7 deve ser 5040', () => {
   expect(fatorial(7)).toBe(5040);
 });
+
+test('fatorial de -1 deve lançar erro', () => {
+  expect(() => fatorial(-1)).toThrow("Fatorial não é definido para números negativos.");
+});
--- src/fat.ts
+++ src/fat.ts
@@ -1,4 +1,7 @@
 export function fatorial(n: number): number {
+    if (n < 0) {
+        throw new Error("Fatorial não é definido para números negativos.");
+    }
     if (n <= 1) {
         return 1;
     }
