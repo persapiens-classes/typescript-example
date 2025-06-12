d1a6c3b13fbd4cb7e057623338c533171d8e1131
Danilo <142433003+daniloladeira@users.noreply.github.com>
2025-06-12 18:33:11 +0000

    fix: Changed string to number

--- src/first-two-elements/first-two-elements.ts
+++ src/first-two-elements/first-two-elements.ts
@@ -1,6 +1,6 @@
-let arr: string[] = [2, 4, 6, 2, 8, 9, 5]
+let arr: number[] = [2, 4, 6, 2, 8, 9, 5]
 
-export function firstTwoElements(arr: string[]) {
+export function firstTwoElements(arr: number[]) {
     let newArray = arr.slice(0, 2)
     return newArray
 }
